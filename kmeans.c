815726c825aa796e9626f69c5d1d6457c364356c
MarcoPSM <sr.marcomartins@gmail.com>
2020-04-13 01:24:28 +0100

    Cumulative Distribution Function

--- kmeans.c
+++ kmeans.c
@@ -1,101 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
+/******************************************
+ * Marco Martins                          *
+ * projeto final de curso Eng.Informática *
+ * algoritmo na pag. 344
+ ******************************************/
 
-/*
-    cc kmeans.c -o kmeans
+#include "dataset.h"
+#include <limits.h>
+/* 
+https://www.tutorialspoint.com/c_standard_library/limits_h.htm 
 */
+
 
 /************************************************************************************/
-/* DATA STRUCT */
+/* HEADERS */
 
-typedef struct Entity {
-    int attribute;
-    struct Entity *next;
-} ENTITY; 
+void initCentroids(DATASET *dataset, int *centroids, int n);
+void listCentroids(int *centroids, int n);
+int getClosetsCentroid(int attribute, int centroids[], int n);
+void kmeans(DATASET *dataset, int k, int e);
 
-typedef ENTITY *DATASET;
+/************************************************************************************/
+/* MAIN FUNCTION */
+int main(int argc, char *argv[]) {
+    DATASET f;
+    int k = 2;
+    int e = 0;
 
+    inic(&f);    
+    loadDatasetFromFile(&f, "datafile.txt");
+//    loadDatasetFromFile(&f, "dataset_img21.txt");
 
-/* FUNCTIONS IMPLEMENTATION */
+    puts("Listar todos");
+    listar(f);
+    printf("A media do dataset é: %d \n", media(&f));
+    printf("A amplitude do dataset é: %d \n", amplitude(&f));
+    printf("A variancia do dataset é: %f \n", variancia(&f));
 
-/*
-    Iniciar DATASET
-*/
-void inic(DATASET *dataset) {
-    *dataset=NULL;
-}
+    kmeans(&f, k, e);
 
-/*
-    Insere um novo registo no fim da dataset
-*/
-void inserir(DATASET *dataset, int value) {
-    if(*dataset==NULL) {
-        *dataset=(DATASET)malloc(sizeof(ENTITY));
-        if(*dataset==NULL) {
-            return;
-        }
-        (*dataset)->attribute=value;
-        (**dataset).next=NULL;
-    }
-    else {
-        inserir(& (**dataset).next, value);
-    }
-}
-
-/*
-    Apagar o primeiro elemento da dataset
-*/
-void apagar(DATASET *dataset) {
-    ENTITY *tmp = *dataset;
-    if(*dataset==NULL) {
-        /*nao ha elementos*/
-        return;
-    }
-    *dataset=(*dataset)->next;
-    free(tmp);
-}
-
-/*
-    Listar todos os elementos de forma recursiva
-*/
-void listar(DATASET dataset) {
-    if (dataset==NULL) {
-        /*nao ha elementos*/
-        return;
-    }
-    printf("Valor: %d \n", dataset->attribute);
-    listar(dataset->next);
-}
-
-int media(DATASET *dataset) {
-    if (dataset==NULL) {
-        /*nao ha elementos*/
-        return 0;
-    }
-    ENTITY *tmp = *dataset;
-    int soma = 0;
-    int i=0;
-    while(tmp != NULL) {
-        soma += (int) tmp->attribute;
-        i++;
-        tmp = tmp->next;
-    }
-    return soma/i;
+    return 0;
 }
 
 /************************************************************************************/
-
 /* FUNCTIONS IMPLEMENTATION */
 
 /*
     funcao para inicializar o vetor de centroids
 */
-void initCentroids(int *centroids, int n) {
+void initCentroids(DATASET *dataset, int *centroids, int n) {
     int value = 2;
     for(int i=0; i<n; i++) {
         centroids[i] = value;
         value *= 2;
     }
+    /* testes */
+    /***************************/
+    int quartil1;
+    int quartil3;
+    quartil1 = quantileFunction(dataset, 0.25);
+    quartil3 = quantileFunction(dataset, 0.75);
+    printf("quartil1= %d quartil3= %d\n", quartil1, quartil3);
+    centroids[0] = quartil1;
+    centroids[1] = quartil3;
+    /***************************/
+    /* dividir o dataset em n+1 partes 
+        (e se quiser mais de 100 centroids ou se o dataset tiver menos elementos que os centroids pretendidos) */
+/*
+    float parte;
+    parte = 100 / (n+1) ;
+    for(int i=0; i<n; i++) {
+        centroids[i] = quantileFunction(dataset, parte);
+        parte+=parte;
+    }
+*/
+}
+
+/*
+    Listar centroids
+*/
+void listCentroids(int *centroids, int n) {
+    printf("Lista de centroids: ");
+    for(int i=0; i<n; i++) {
+        printf("%d ", centroids[i]);
+    }
+    printf("\n");
 }
 
 /*
@@ -122,6 +110,8 @@ int getClosetsCentroid(int attribute, int centroids[], int n) {
     return colsestCentroid;
 }
 
+
+
 void kmeans(DATASET *dataset, int k, int e) {
     /*iterador*/
     int t=0;
@@ -130,14 +120,10 @@ void kmeans(DATASET *dataset, int k, int e) {
     /*lista de clusters*/
     DATASET clusters[k];
     int aux;
-    int soma_dist_novos_centros_aos_antigos;
+    int delta;
 
-    /*****   VER A CENA DE TROCAR INTEIROS  *****/
-    initCentroids(centroids, k);
-    /*output dos centroids*/
-    for(int i=0; i<k; i++) {
-        printf("%d \n", centroids[i]);
-    }
+    initCentroids(dataset, centroids, k);
+    listCentroids(centroids, k);
 
     do {
         t += 1;
@@ -149,70 +135,28 @@ void kmeans(DATASET *dataset, int k, int e) {
         /*atribuicao de clusters*/
         ENTITY *tmp = *dataset;
         while(tmp != NULL) {
-            printf("Valor: %d \n", tmp->attribute);
             aux = getClosetsCentroid(tmp->attribute, centroids, k);
             inserir(&clusters[aux], tmp->attribute);
-            printf("cluster: %d \n", aux);
-
             tmp=tmp->next;
         }
 
         /*atualizar os centroids*/
-        soma_dist_novos_centros_aos_antigos = 0;
+        delta = 0;
         for(int i=0; i<k; i++) {
-            printf("Listar cluster %d \n", i);
-            listar(clusters[i]);
             aux = media(&clusters[i]);
-            printf("Media do cluster %d \n", aux);
-            soma_dist_novos_centros_aos_antigos += (centroids[i] < aux) ? aux-centroids[i] : centroids[i] - aux;
+            delta += (centroids[i] < aux) ? aux-centroids[i] : centroids[i] - aux;
             centroids[i] = aux;
         }
         
-        printf("soma_dist_novos_centros_aos_antigos %d \n", soma_dist_novos_centros_aos_antigos);
-    } while( soma_dist_novos_centros_aos_antigos > e );    
-    
-}
+        printf("delta %d \n", delta);
+    } while( delta > e );  
+    printf("Iteracaoes: %d\n", t);  
 
-
-/************************************************************************************/
-/* MAIN FUNCTION */
-int main(int argc, char *argv[]) {
-    DATASET f;
-    int k = 2;
-    int e = 0;
-    int testing = 0; // true=1 false=0
-    
-    inic(&f);
-    inserir(&f, 2);
-    inserir(&f, 3);
-    inserir(&f, 4);
-    inserir(&f, 10);
-    inserir(&f, 11);
-    inserir(&f, 12);
-    inserir(&f, 20);
-    inserir(&f, 25);
-    inserir(&f, 30);
-    puts("Listar todos");
-    listar(f);
-    printf("A media do dataset é: %d \n", media(&f));
-    
-    if (testing) {
-        apagar(&f);
-        puts("Listar menos 1");
-        listar(f);
-        apagar(&f);
-        puts("Listar menos 2");
-        listar(f);
-        apagar(&f);
+            
+    for(int i=0; i<k; i++) {
+        printf("Listar cluster %d \n", i);
+        listar(clusters[i]);
+        printf("Media do cluster %d \n", media(&clusters[i]));
     }
-    else {
-        kmeans(&f, k, e);
-        puts("Listar todos");
-        listar(f);
-    }
-
-    return 0;
-
 }
-
 
